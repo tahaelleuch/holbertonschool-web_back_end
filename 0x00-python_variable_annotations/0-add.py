cec020d23838acb5f812aae3220b61554aa0b570
Taha Elleuch <Tahaeleuch@gmail.com>
2021-03-16 22:33:49 +0100

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""add two float"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add two float
+    Args:
+        a (float): [first float]
+        b (float): [second float]
+    Returns:
+        float: a +b
+    """
+    return a + b
