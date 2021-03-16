a2cb59df7bca14ead28e5897e1d3e6d09674cc56
Taha Elleuch <Tahaeleuch@gmail.com>
2021-03-16 22:40:30 +0100

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""concat two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat two strings
+    Args:
+        str1: string one
+        str2: string two
+
+    Return:
+        str1 + str2
+    """
+    return str1 + str2
