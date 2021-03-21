894be67908df653af51a49f7c83e4857372383a7
Taha Elleuch <Tahaeleuch@gmail.com>
2021-03-21 21:56:37 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""2-floor.py"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floar
+
+    Args:
+        n(float): float
+
+    Returns:
+        int: res
+    """
+    return math.floor(n)
