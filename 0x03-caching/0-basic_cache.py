5f5c64b7d57c70ad72200d403997a67d71f2f77a
Taha Elleuch <Tahaeleuch@gmail.com>
2021-03-22 22:59:16 +0100

    caching

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,10 +9,22 @@ class BasicCache(BaseCaching):
     basiccash
     """
     def put(self, key, item):
-        """put funcs"""
+        """put funcs
+
+        Args:
+            key(str): key to the dict
+            item(str): value to be assigned
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get function"""
+        """get function
+
+        Args:
+            key(str): key to the dict
+
+        Returns:
+            str or None: get value by key
+        """
         return self.cache_data.get(key) or None
