04e94d39618ed21afc4996b80f73056d98f469b5
Taha Elleuch <Tahaeleuch@gmail.com>
2021-03-31 20:39:14 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,18 @@ class Auth:
         Returns:
             False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None and not excluded_paths:
+            return True
+
+        if path[-1] != "/":
+            path = path + "/"
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
