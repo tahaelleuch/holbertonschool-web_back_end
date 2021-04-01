62e606e1866cb812fa957a19308a2bf087b990f3
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-01 22:47:13 +0100

    basic

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ basic auth
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -27,3 +28,24 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decoded
+        Args:
+            base64_authorization_header(string): decoded
+        """
+
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64_authorization_header.encode('utf-8')
+            return base64.b64decode(decoded).decode('utf-8')
+        except Exception:
+            return None
