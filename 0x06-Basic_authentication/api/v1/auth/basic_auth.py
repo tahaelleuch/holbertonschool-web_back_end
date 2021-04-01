a31d64ac7f119f4c67654364fb45d5fa52b46abf
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-01 22:55:21 +0100

    basic

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,3 +49,20 @@ class BasicAuth(Auth):
             return base64.b64decode(decoded).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        extract credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        credentials = decoded_base64_authorization_header.split(':')
+
+        return (credentials[0], credentials[1])
