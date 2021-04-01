9c3c1edfce7fa1fe4a58c88742aa42c95779dd2f
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-01 22:31:07 +0100

    basic

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,25 @@ basic auth
 
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """
     BasicAuth
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extract
+        Args:
+            authorization_header(string): the authorization_heade
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+
+        if len(authorization_header) < 7 \
+                or authorization_header[:6] != "Basic ":
+            return None
+
+        return authorization_header[6:]
