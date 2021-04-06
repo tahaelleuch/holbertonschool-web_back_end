20ad570cc2657f1ca2766af8ba2867e9e5969b9f
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-06 22:52:51 +0100

    exp

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif os.getenv("AUTH_TYPE") == "session_auth":
     auth = SessionAuth()
+elif os.getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
 elif os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 else:
---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""expiration date for session"""
+
+from api.v1.auth.session_auth import SessionAuth
+import os
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """expired session"""
+
+    def __init__(self):
+        """init"""
+        if not os.getenv('SESSION_DURATION'):
+            self.session_duration = 0
+        else:
+            try:
+                self.session_duration = int(os.getenv('SESSION_DURATION'))
+            except Exception:
+                self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """create session"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {}
+        session_dictionary["user_id"] = user_id
+        session_dictionary["created_at"] = datetime.now()
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """user"""
+        if session_id is None:
+            return None
+
+        if session_id not in self.user_id_by_session_id:
+            return None
+
+        session_dict = self.user_id_by_session_id[session_id]
+
+        if self.session_duration <= 0:
+            return session_dict["user_id"]
+
+        if "created_at" not in session_dict:
+            return None
+
+        t = timedelta(seconds=self.session_duration)
+        delta_time = t + session_dict["created_at"]
+
+        if delta_time < datetime.now():
+            return None
+        return session_dict["user_id"]
