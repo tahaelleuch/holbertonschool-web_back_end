abc8b72e81f6bea32d70df8a4b5ed84a9a08b917
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-06 21:36:24 +0100

    fix

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -64,12 +64,18 @@ def before_request():
     if not auth.require_auth(request.path, key_paths):
         return
 
-    if auth.current_user(request) is None:
-        abort(403)
+    if os.getenv("AUTH_TYPE") == "auth" or \
+            os.getenv("AUTH_TYPE") == "basic_auth":
+        if auth.authorization_header(request) is None:
+            abort(401)
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
+    if os.getenv("AUTH_TYPE") == "session_auth":
+        if auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
     request.current_user = auth.current_user(request)
 
