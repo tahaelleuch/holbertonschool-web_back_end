70e04a4b47ac977fe24ef4c016bc908ce46d82c5
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-14 18:17:49 +0100

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -33,3 +33,15 @@ class Auth:
         pwd = _hash_password(password)
         new_user = self._db.add_user(email, pwd)
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode(),
+                user.hashed_password
+            )
+        except NoResultFound:
+            return False
+
