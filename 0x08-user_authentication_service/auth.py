61a03b8e80a4682d5f016361d7de50bf22fef272
Taha Elleuch <Tahaeleuch@gmail.com>
2021-04-14 17:32:36 +0100

    auth.py

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+def _hash_password(password: str) -> str:
+    """crypt password"""
+    crypted = bcrypt.hashpw(
+        password.encode('utf-8'),
+        bcrypt.gensalt()
+    )
+    return crypted
