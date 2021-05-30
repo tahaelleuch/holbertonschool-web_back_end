e154cd37fd24b12f5d414b7593cea3de34e7c3cb
Taha Elleuch <Tahaeleuch@gmail.com>
2021-05-30 11:52:13 +0100

    web.py

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""web"""
+
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+r = redis.Redis()
+
+
+def count_req(method: Callable) -> Callable:
+    """counr request"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper"""
+        r.incr("count:{}".format(url))
+        cached_html = r.get("cached:{}".format(url))
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        r.setex("cached:{}".format(url), 10, html)
+        return html
+
+    return wrapper
+
+def get_page(url: str) -> str:
+    """get page"""
+    req = requests.get(url)
+    return req.text
