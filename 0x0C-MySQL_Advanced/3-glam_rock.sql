e84688185b400b42c413c5dc713cd7b6ff78fc63
Taha Elleuch <tahaeleuch@gmail.com>
2021-05-30 13:18:43 +0000

    3-glam_rock.sql

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- duration of activity
-SELECT band_name, (IFNULL(split, 2021) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
 
