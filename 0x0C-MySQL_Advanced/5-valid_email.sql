38f162c5012c9d5758912ad08aed5024b39c34e6
Taha Elleuch <tahaeleuch@gmail.com>
2021-05-30 14:52:22 +0000

    5-valid_email.sql

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- create validation email trigger
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
