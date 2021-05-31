89acb53734de192f7a1d9eb2be2e1d708e88911f
Taha Elleuch <Tahaeleuch@gmail.com>
2021-06-01 00:28:04 +0100

    promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled(
+    [uploadPhoto(fileName), signUpUser(firstName, lastName)]
+  );
+}
+
+export default handleProfileSignup;
